c0494a3fb4ea54668f3d4b5d29fd2e564ea7f5cc
tais <tais.enge.ska@gmail.com>
2026-05-12 20:04:59 -0300

    Adicionando as copias dos arquivios

---
+++ src - Copia/jogoindex.ts
@@ -0,0 +1,9 @@
+import { guer } from "./guerreiro.ts";
+import { jogo } from "./jogo.ts";
+import { Mago } from "./mago.ts";
+
+let mago: Mago = new Mago("𝖒𝖆𝖌𝖔", 200, 200);
+let guerer: guer = new guer("𝖌𝖚𝖊𝖗𝖗𝖊𝖎𝖗𝖔", 200, 200);
+
+let game: jogo = new jogo();
+game.inicial(mago, guerer);
